3c1c8f1de4daf417c0707a470c1037d16c6437df
MdTasib <ohidulalam442@gmail.com>
2023-07-07 16:13:25 +0600

    envoirment section complete

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./header";
 import Collection from "./collection";
 import Stylish from "./stylish";
+import Envoirment from "./envoirment";
 
 const Home = () => {
 	return (
@@ -9,6 +10,7 @@ const Home = () => {
 			<Header />
 			<Collection />
 			<Stylish />
+			<Envoirment />
 		</main>
 	);
 };
