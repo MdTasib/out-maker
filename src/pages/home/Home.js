78b9c64cecc240c2d5c4dfc870947720e5ac3698
MdTasib <ohidulalam442@gmail.com>
2023-07-07 12:17:57 +0600

    Our Popular Collection section complete

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./header";
+import Collection from "./collection";
 
 const Home = () => {
 	return (
 		<main>
 			<Header />
+			<Collection />
 		</main>
 	);
 };
--- src/shared/Button/Button.js
+++ src/shared/Button/Button.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Button = ({ children, className }) => {
-	return <button className={className}>{children}</button>;
+	return <button className={`${className} text-xs`}>{children}</button>;
 };
 
 export default Button;
