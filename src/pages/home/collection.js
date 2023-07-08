ae132fc495f5319b8a95f640d935ef0e3d036fcc
MdTasib <ohidulalam442@gmail.com>
2023-07-08 18:38:39 +0600

    css issu fix

--- src/pages/home/collection.js
+++ src/pages/home/collection.js
@@ -24,22 +24,24 @@ const Collection = () => {
 				</Link>
 			</div>
 
-			<div class='block md:flex justify-center items-center mt-4 gap-4'>
-				<div class='flex items-center bg-[#e2fafd] p-5 rounded-xl my-2'>
+			<div class='grid grid-cols-1 lg:grid-cols-2 mt-4 gap-4'>
+				<div class='block md:flex items-center bg-[#e2fafd] p-5 rounded-xl my-2'>
 					<div class='relative'>
 						<img
 							className='absolute h-14 w-48 bottom-36 right-12'
 							src={loungeVactor}
 							alt=''
 						/>
-						<h6 class='text-primary text-2xl font-semibold'>Chaise Lounge</h6>
-						<p class='text-sm font-normal leading-6 py-3'>
-							We are small family business in Europe who love to make
-							exceptional printed products!
-						</p>
-						<Button className='btn btn-primary rounded-full text-white hover:text-primary hover:bg-white'>
-							Show Now <BsArrowRight className='ms-2' />
-						</Button>
+						<div className='text-center md:text-left mb-4 md:mb-4'>
+							<h6 class='text-primary text-2xl font-semibold'>Chaise Lounge</h6>
+							<p class='text-sm font-normal leading-6 py-3'>
+								We are small family business in Europe who love to make
+								exceptional printed products!
+							</p>
+							<Button className='btn btn-primary rounded-full text-white hover:text-primary hover:bg-white'>
+								Show Now <BsArrowRight className='ms-2' />
+							</Button>
+						</div>
 					</div>
 					<div class='flex justify-center items-center'>
 						<div className='bg-[#A5E4EC] w-52 h-52 rounded-full flex justify-center items-center p-4'>
@@ -47,8 +49,8 @@ const Collection = () => {
 						</div>
 					</div>
 				</div>
-				<div class='flex items-center bg-[#FFF5E4] p-5 rounded-xl my-2'>
-					<div class=''>
+				<div class='block md:flex items-center bg-[#FFF5E4] p-5 rounded-xl my-2'>
+					<div class='text-center md:text-left mb-4 md:mb-4'>
 						<h6 class='text-primary text-2xl font-semibold'>Harriet Ottoman</h6>
 						<p class='text-sm font-normal leading-6 py-3'>
 							We are small family business in Europe who love to make
