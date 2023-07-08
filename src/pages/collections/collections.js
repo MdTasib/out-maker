f453bc524d3fbdbf424555820f464d3d4b2cbdc0
MdTasib <ohidulalam442@gmail.com>
2023-07-08 22:19:46 +0600

    single blog

--- src/pages/collections/collections.js
+++ src/pages/collections/collections.js
@@ -1,10 +1,12 @@
 import React from "react";
 import PageNav from "./PageNav";
+import Blogs from "./blogs";
 
 const Collections = () => {
 	return (
 		<main className='container mx-auto px-10'>
 			<PageNav />
+			<Blogs />
 		</main>
 	);
 };
