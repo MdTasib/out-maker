5373b2b72a89be032f2806c2bba087d04a1f78c8
MdTasib <ohidulalam442@gmail.com>
2023-07-09 17:52:03 +0600

    blog page complte

--- src/pages/blog/Blog.js
+++ src/pages/blog/Blog.js
@@ -1,10 +1,16 @@
 import React from "react";
 import BlogHeader from "./BlogHeader";
+import Posts from "./Posts";
+import Articels from "./Articels";
+import Network from "../../shared/Network/Network";
 
 const Blog = () => {
 	return (
 		<main className='container mx-auto px-10'>
 			<BlogHeader />
+			<Posts />
+			<Articels />
+			<Network />
 		</main>
 	);
 };
