e11a4ab66aa64c8bd4148e4c89a10b9a7ddfa06b
MdTasib <ohidulalam442@gmail.com>
2023-07-09 17:10:57 +0600

    blog page heading complte

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./shared/Navbar/Navbar";
 import Collections from "./pages/collections/collections";
 import Services from "./pages/home/services";
 import Footer from "./shared/Footer/Footer";
+import Blog from "./pages/blog/Blog";
 
 function App() {
 	return (
@@ -13,6 +14,7 @@ function App() {
 			<Routes>
 				<Route path='/' element={<Home />} />
 				<Route path='/collections' element={<Collections />} />
+				<Route path='/blog' element={<Blog />} />
 			</Routes>
 			<Services />
 			<Footer />
---
+++ src/pages/blog/Blog.js
@@ -0,0 +1,12 @@
+import React from "react";
+import BlogHeader from "./BlogHeader";
+
+const Blog = () => {
+	return (
+		<main className='container mx-auto px-10'>
+			<BlogHeader />
+		</main>
+	);
+};
+
+export default Blog;
