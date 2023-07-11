7872b5c8b3e1ba87e6063b86cafd2b113eeb2a17
MdTasib <ohidulalam442@gmail.com>
2023-07-11 18:52:37 +0600

    feature

--- src/pages/all-blogs/PageNav.js
+++ src/pages/all-blogs/PageNav.js
@@ -3,13 +3,13 @@ import React from "react";
 const PageNav = () => {
 	return (
 		<>
-			<nav className='py-4'>
+			<nav className='pt-4'>
 				<ul className='flex text-sm text-[#000000] font-normal'>
 					<li className='me-6 pb-1 border-b-2 border-b-black'>Home</li>
 					<li className='font-semibold'>Blogs</li>
 				</ul>
 			</nav>
-			<nav className=''>
+			<nav className='py-8'>
 				<ul className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 justify-between text-xl text-primary font-bold text-center'>
 					<li className='cursor-pointer bg-gray-100 hover:text-white py-4 hover:bg-primary'>
 						All
