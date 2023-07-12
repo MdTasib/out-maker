92b3434ef3ca8d63846b96f85032ad5022bf7043
MdTasib <ohidulalam442@gmail.com>
2023-07-12 17:17:51 +0600

    add min icon

--- src/pages/blog/Blog.js
+++ src/pages/blog/Blog.js
@@ -3,14 +3,19 @@ import BlogHeader from "./BlogHeader";
 import Posts from "./Posts";
 import Articels from "./Articels";
 import Network from "../../shared/Network/Network";
+import mic from "../../assets/images/mic.png";
 
 const Blog = () => {
 	return (
-		<main className='container mx-auto px-10'>
+		<main className='container mx-auto px-10 relative'>
 			<BlogHeader />
 			<Posts />
 			<Articels />
 			<Network />
+
+			<div className='absolute right-0 bottom-[0px]'>
+				<img src={mic} alt='' />
+			</div>
 		</main>
 	);
 };
--- src/pages/collections/collections.js
+++ src/pages/collections/collections.js
@@ -2,13 +2,18 @@ import React from "react";
 import PageNav from "./PageNav";
 import Blogs from "./blogs";
 import Network from "../../shared/Network/Network";
+import mic from "../../assets/images/mic.png";
 
 const Collections = () => {
 	return (
-		<main className='container mx-auto px-10'>
+		<main className='container mx-auto px-10 relative'>
 			<PageNav />
 			<Blogs />
 			<Network />
+
+			<div className='absolute right-0 bottom-[0px]'>
+				<img src={mic} alt='' />
+			</div>
 		</main>
 	);
 };
